620ff5a9c913cbf3fbc5f1162d2d9d0b1191a139
alizayzulfiqar110-lang <alizayzulfiqar110@gmail.com>
2026-06-17 02:10:35 +0500

    Add dynamic greeting based on current hour

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const hour = new Date().getHours();
+let greeting;
 
+if (hour < 12) {
+  greeting = "Good morning! Welcome to my portfolio.";
+} else if (hour < 18) {
+  greeting = "Good afternoon! Thanks for visiting.";
+} else {
+  greeting = "Good evening! Glad you stopped by.";
+}
+
+document.getElementById("greeting").textContent = greeting;
